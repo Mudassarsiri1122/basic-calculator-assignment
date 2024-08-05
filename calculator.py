ab2617d0b0d363f8c8a5ec854b04ee7a004e748f
Mudassarsiri1122 <carolinewalker262@gmail.com>
2024-08-05 04:29:20 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#addition
+number1 = 2
+number2 =5
+add_result =number1+number2
+#Subtract 
+number1 = 2
+number2 = 5
+subtract_result = number1-number2
+#Multiplicaton 
+number1 = 2
+number2 = 5
+multiply_result = number1*number2
+#Division
+number1 = 2
+number2 = 5
+divide_result = number1/number2
+#Print Variables
+print(add_result,subtract_result,multiply_result,divide_result)
